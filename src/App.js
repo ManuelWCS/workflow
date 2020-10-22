f3dc9bba23daf584e7cf4f187a056acf6216f7df
FlorianBEME <florianbme@gmail.com>
2020-10-22 16:11:12 +0200

    add new beautiful header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         ICI MON SUPER HEADER!!!!
         </p>
         <a
           className="App-link"
